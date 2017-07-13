7fc8fd1d2a4e8a36887c45a9146962378d2b0ebf
benmeign <benedicte.meignan@gmail.com>
2017-07-13 09:29:28 +0200

    last version before fine-tuning changes

--- SeasonGame.js
+++ SeasonGame.js
@@ -2,15 +2,7 @@ var SeasonGame = function() {
   this.vegetablesList = _.shuffle(vegetables);
   this.selectedVegetable = null;
   this.init();
-  this.score;
-};
-
-var sumOfArray = function(array,indexStart,indexEnd) {
-  var sum = 0;
-  for(var i = indexStart;i<=indexEnd;i++){
-      sum += array[i];
-  }
-  return sum;
+  this.score = 0;
 };
 
 SeasonGame.prototype.init = function () {
@@ -27,48 +19,34 @@ SeasonGame.prototype.init = function () {
 
   $(".seasonsOptions").click(function(){
     that.selectedVegetable.months;
+    var points = that.selectedVegetable.difficultyRange;
+    that.score;
     var seasonVal = $(this).attr("val");
 
     var isWinterCorrect = (sumOfArray(that.selectedVegetable.months,0,2)>=1);
     var isSpringCorrect = (sumOfArray(that.selectedVegetable.months,3,5)>=1);
     var isSummerCorrect = (sumOfArray(that.selectedVegetable.months,6,8)>=1);
     var isAutomnCorrect = (sumOfArray(that.selectedVegetable.months,9,11)>=1);
-  
+
     if ((seasonVal === "winter" && isWinterCorrect) ||
           (seasonVal === "spring" && isSpringCorrect)  ||
           (seasonVal === "summer" && isSummerCorrect) ||
           (seasonVal === "automn" && isAutomnCorrect)) {
             console.log("You're a star");
+            // setTimeout(function() { prompt("You're a star"); }, 1);
+            that.score += points;
           }
           else {
             console.log("Too bad");
     }
+    $('.points').text(that.score);
   });
-
-
 };
 
-
-SeasonGame.prototype.play = function () {
-
+var sumOfArray = function(array,indexStart,indexEnd) {
+  var sum = 0;
+  for(var i = indexStart; i<=indexEnd; i++) {
+      sum += array[i];
+  }
+  return sum;
 };
-
-
-
-  // The user clicks on the associated season and gets points depending on how right he is. He can be :
-  // - right 100% (there is only 1 season and he gets it right or there are 2 to 4 right seasons and he gets 1 right)
-  // - right 50% (there are 1 or more full season(s) and he picks a month in an uncomplete season)
-  // - right 0% (he doesn't select a right month)
-
-
-  function choseSeason (){
-
-
-
-  }
-
-  // The user clicks on the associated season and gets points depending on how right he is. He can be :
-
-  function correctAnswer(){
-
-  }
